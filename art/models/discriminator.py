be7a411a71d1f834e8998c3a16ebcd496bf90d82
Mark <markjimenez3@gmail.com>
2021-09-09 11:39:10 -1000

    defined discriminator

---
+++ art/models/discriminator.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import layers
+
+
+class Discriminator(tf.keras.Model):
+
+    def __init__(self):
+
+        super(Discriminator, self).__init__()
+
+if __name__ == "__main__":
+
+    print("hello")
