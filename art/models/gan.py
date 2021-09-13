7660a3cff08d6b4ced89e87770f1a4195ce6d80a
Mark <markjimenez3@gmail.com>
2021-09-12 22:25:01 -1000

    working on painter AI

--- art/models/gan.py
+++ art/models/gan.py
@@ -12,12 +12,12 @@ class Discriminator(tf.keras.Model):
 
         self.inputLayer = layers.InputLayer(inputShape)
 
-        self.convLayer1 = layers.Conv2D(64, (5,5), strides=(2,2), padding='same',
-                input_shape = [28,28,1])
+        self.convLayer1 = layers.Conv2D(2, (5,5), strides=(2,2), padding='same')
+
         self.reluLayer1 = layers.LeakyReLU()
         self.dropoutLayer1 = layers.Dropout(0.3)
 
-        self.convLayer2 = layers.Conv2D(128, (5,5), strides=(2,2), padding='same')
+        self.convLayer2 = layers.Conv2D(4, (5,5), strides=(2,2), padding='same')
         self.reluLayer2 = layers.LeakyReLU()
         self.dropoutLayer2 = layers.Dropout(0.3)
 
@@ -26,6 +26,8 @@ class Discriminator(tf.keras.Model):
 
         self.cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
         self.optimizer = tf.keras.optimizers.Adam(1e-4)
+
+        print('Initialized Discriminator...',flush=True)
 
     def call(self, x):
 
@@ -55,30 +57,31 @@ class Generator(tf.keras.Model):
         super(Generator, self).__init__()
 
         self.inputLayer = layers.InputLayer(inputShape)
-        self.dense = layers.Dense(7*7*256, use_bias=False, input_shape=(100,))
+        #self.dense = layers.Dense(3*3*256, use_bias=False)
 
         self.batchNormLayer1 = layers.BatchNormalization()
         self.reluLayer1 = layers.LeakyReLU()
-        self.reshape1 = layers.Reshape((7,7,256))
-        self.convLayer1 = layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)
+        self.reshape1 = layers.Reshape((200, 200, 3))
+        self.convLayer1 = layers.Conv2DTranspose(4, (5, 5), strides=(1, 1), padding='same', use_bias=False)
 
         self.batchNormLayer2 = layers.BatchNormalization()
         self.reluLayer2 = layers.LeakyReLU()
-        self.convLayer2 = layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False)
+        self.convLayer2 = layers.Conv2DTranspose(2, (5, 5), strides=(2, 2), padding='same', use_bias=False)
 
         self.batchNormLayer3 = layers.BatchNormalization()
         self.reluLayer3 = layers.LeakyReLU()
 
         self.convLayer3 = layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh')
 
-
         self.cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
         self.optimizer = tf.keras.optimizers.Adam(1e-4)
+
+        print('Initialized Generator...',flush=True)
 
     def call(self, x, training=False):
 
         x = self.inputLayer(x)
-        x = self.dense(x)
+        #x = self.dense(x)
         x = self.batchNormLayer1(x, training=training) 
         x = self.reluLayer1(x)
         x = self.reshape1(x)
@@ -102,13 +105,17 @@ class GAN:
         self.generator = generator
         self.discriminator = discriminator
 
-        self.noiseDim = 100
+        self.channels = 3 
+        self.imageWidth = 200
+        self.imageHeight = 200
         self.numSamplesToGenerate = 16
-        self.seed = tf.random.normal([self.numSamplesToGenerate, self.noiseDim])
+        self.seed = tf.random.normal([self.numSamplesToGenerate, self.imageHeight, self.imageWidth, self.channels])
         self.batchSize = 16 
         self.epochs = 10
 
-    def _generate_images(self, epoch, testInput):
+    def _generate_images(self, epoch):
+
+        testInput = self.seed
 
         predictions = self.generator(testInput)
 
@@ -117,14 +124,14 @@ class GAN:
         for i in range(predictions.shape[0]):
 
             plt.subplot(4, 4, i+1)
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5)
             plt.axis('off')
 
         plt.show()
 
     def _train_step(self, images):
 
-        noise = tf.random.normal([self.batchSize, self.noiseDim])
+        noise = self.seed 
 
         with tf.GradientTape() as genTape, tf.GradientTape() as discTape:
 
@@ -146,12 +153,14 @@ class GAN:
 
         for epoch in range(self.epochs):
 
+            print('Epoch Number {}'.format(epoch))
+
             start = time.time()
 
             for imageBatch in dataset:
 
                 self._train_step(imageBatch)
 
-            self._generate_images(epoch + 1, self.seed)
+            self._generate_images(epoch + 1)
 
-        self._generate_images(self.epochs, self.seed)
+        self._generate_images(self.epochs)
--- art/models/train.py
+++ art/models/train.py
@@ -4,10 +4,60 @@ import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import sys
 import PIL
 import tensorflow as tf
 from tensorflow.keras import layers
 import time
+import csv
+import tqdm
+
+def convert_row_to_image_data(row):
+
+    image = []
+    pixelValue = ''
+
+    for item in row:
+
+        for char in item:
+
+            if char == '[' or char == ' ':
+                continue
+            elif char == ',' or char == ']':
+                if len(pixelValue) > 0:
+                    image.append(float(pixelValue))
+                    pixelValue = ''
+                continue
+            else:
+                pixelValue += char
+
+    image = np.array(image).reshape((200,200,3))
+
+    return image
+
+def get_artist_paintings(artistName):
+
+    paintingsFileName = os.path.join('../data/artists/', '{}.csv'.format(artistName))
+    print('reading artist paintings {}'.format(artistName), flush=True)
+
+    images = []
+
+    with open(paintingsFileName, 'r') as f:
+
+        reader = csv.reader(f)
+
+        imgNum = 0
+        for row in reader:
+
+            imgNum += 1
+            print('reading image {}'.format(imgNum))
+
+            image = convert_row_to_image_data(row) 
+            images.append(image)
+            if imgNum == 16:
+                break
+
+    return np.array(images)
 
 """
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
@@ -15,16 +65,26 @@ train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('fl
 train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 """
 
-BUFFER_SIZE = 60000
-BATCH_SIZE = 256
-# Batch and shuffle the data
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+def main(artistName):
 
-inputShape = train_images.shape[1:]
+    trainImages = get_artist_paintings(artistName)
+    print(trainImages.shape, flush=True)
 
-discriminator = gan.Discriminator(inputShape)
-generator = gan.Generator(inputShape)
-gan = gan.GAN(generator, discriminator)
+    # Batch and shuffle the data
+    BUFFER_SIZE = 60000
+    BATCH_SIZE = 16 
+    trainDataset = tf.data.Dataset.from_tensor_slices(trainImages).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
-gan.train(train_dataset)
+    inputShape = trainImages.shape[1:]
+
+    discriminator = gan.Discriminator(inputShape)
+    generator = gan.Generator(inputShape)
+    adversarialPair = gan.GAN(generator, discriminator)
+
+    adversarialPair.train(trainDataset)
+
+if __name__ == '__main__':
+
+    artistName = 'Michelangelo'
+    main(artistName)
 
