60305d30165e5e3c7f7c70360e3e3646b8029425
Mark <markjimenez3@gmail.com>
2021-09-12 17:35:51 -1000

    working on painting dataset

--- art/models/train.py
+++ art/models/train.py
@@ -9,42 +9,12 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import time
 
-def load_images():
-
-    imagesDir = "../data/images/images/"
-    artists = os.listdir(imagesDir)
-
-    dataset = []
-
-    for artist in artists:
-
-        artistFolder = os.path.join(imagesDir, artist)
-        images = os.listdir(artistFolder)
-
-        for imageName in images:
-
-            imageName = os.path.join(artistFolder, imageName)
-
-            img = PIL.Image.open(imageName)
-            shape = img.size + (3,)
-
-            data = np.array(list(img.getdata())).reshape(shape)
-            dataset.append(data)
-            print(data.shape, flush=True)
-
-            img.close()
-
-        print(np.array(dataset).shape, flush=True)
-
-
 """
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
 train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 """
 
-load_images()
-exit(1)
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
 # Batch and shuffle the data
